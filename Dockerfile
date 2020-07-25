f3faf8805fd48b3430d6ca75b826d9a7317ec741
turneps403 <turneps403@mail.ru>
2020-07-26 00:02:22 +0300

    HWA-01
    - working version

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM openjdk:15-jdk-alpine
-LABEL description="just for example"
-RUN addgroup -S otusdemo && adduser -S otusspring -G otusdemo
-USER otusspring:otusdemo
+LABEL description="it is spring web server for otus homework"
+#RUN addgroup -S otusdemo && adduser -S otusspring -G otusdemo
+#USER otusspring:otusdemo
 WORKDIR /usr/local/myapp
-EXPOSE 8080
-ARG JAR_FILE=target/demo-0.0.1-SNAPSHOT.jar
+EXPOSE 8000
+ARG JAR_FILE=target/demo-0.0.2-SNAPSHOT.jar
 COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java","-jar","/usr/local/myapp/app.jar"]
+ENTRYPOINT ["java","-jar","/usr/local/myapp/app.jar", "--server.port=8000"]
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
@@ -20,8 +24,19 @@ public class DemoApplication {
         return String.format("Hello %s!", name);
     }
 
-    @RequestMapping("/ping")
-    public String ping() {
-        return "pong";
+    @RequestMapping("/health")
+    public Map health() {
+        return Collections.singletonMap("status", "OK");
     }
+
+    @RequestMapping("/ping")
+    public Map ping() {
+        return Collections.singletonMap("res", "pong");
+    }
+
+    @RequestMapping(value = "/isready", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map isready() {
+        return Collections.singletonMap("res", "yes");
+    }
+
 }
